35648f1e24bd7fad96d9a96bacab668314c005f0
Damir Miric <damir.miric@gmail.com>
2025-06-28 09:11:09 +0200

    Remove V1 Products

--- service/routers/v1.py
+++ service/routers/v1.py
@@ -2,13 +2,11 @@ from fastapi import APIRouter, Depends
 from service.routers.auth import RequireAuth
 
 # Import the new routers
-from .v1 import products
 from .v1 import stores
 from .v1 import users
 
 router = APIRouter(tags=["API v1"], dependencies=[Depends(RequireAuth)])
 
 # Include the routers from the new files
-router.include_router(products.router)
 router.include_router(stores.router)
 router.include_router(users.router)
