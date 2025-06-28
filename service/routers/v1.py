b3552e5b86897c9e7309b3846b192b7c24ca7249
Damir Miric <damir.miric@gmail.com>
2025-06-28 09:07:25 +0200

    Remove V1 Chat

--- service/routers/v1.py
+++ service/routers/v1.py
@@ -2,7 +2,6 @@ from fastapi import APIRouter, Depends
 from service.routers.auth import RequireAuth
 
 # Import the new routers
-from .v1 import chat
 from .v1 import products
 from .v1 import stores
 from .v1 import users
@@ -10,7 +9,6 @@ from .v1 import users
 router = APIRouter(tags=["API v1"], dependencies=[Depends(RequireAuth)])
 
 # Include the routers from the new files
-router.include_router(chat.router)
 router.include_router(products.router)
 router.include_router(stores.router)
 router.include_router(users.router)
